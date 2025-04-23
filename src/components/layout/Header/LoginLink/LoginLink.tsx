a3254182121f2f2236466bbbf4e1056ade1b1e7e
Kyeong Hoon Chu <khchu1220@naver.com>
2025-04-23 15:02:05 +0900

    fix: merge conflict 해결

--- src/components/layout/Header/LoginLink/LoginLink.tsx
+++ src/components/layout/Header/LoginLink/LoginLink.tsx
@@ -13,12 +13,7 @@ export default function LoginLink({ href, text }: LoginButtonProps) {
       href={href}
       className="font-NanumSquareRoundB flex items-center gap-[0.25rem] text-[1.125rem] whitespace-nowrap text-[#666]"
     >
-      <Image
-        src={ProfileIcon}
-        alt="로그인 페이지 이동"
-        width={20}
-        height={20}
-      />
+      <ProfileIcon />
       {text}
     </Link>
   );
