ae13f516657ac67b1bd5f37e42a1c82a8f0f39db
Kyeong Hoon Chu <khchu1220@naver.com>
2025-04-24 08:10:24 +0900

    fix: mbti page 작은모바일 화면 대응

--- src/app/mbti/layout.tsx
+++ src/app/mbti/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <section className="mx-auto mt-[6.25rem] flex w-full flex-col items-center px-[20px]">
+    <section className="flex h-[calc(100vh-64px)] w-full flex-col items-center justify-center px-4 md:h-[calc(100vh-80px)]">
       {children}
     </section>
   );
--- src/app/mbti/page.tsx
+++ src/app/mbti/page.tsx
@@ -34,18 +34,19 @@ export default function MbtiPage() {
           </p>
         ))}
       </div>
-      <div className="mb-[18px] w-[350px] md:mb-[7px] md:w-[400px]">
+      <div className="max-xxs:w-full mb-[18px] w-[350px] md:mb-[7px] md:w-[400px]">
         <Image
           className="w-full"
           src={MbtiIndexImage}
           alt={title}
           width={400}
           height={400}
+          loading="lazy"
         />
       </div>
       <Link
         href={href}
-        className="font-HappinessB bg-primary rounded-[50px] px-[129px] py-[8px] text-[0.875rem] whitespace-nowrap text-[#fff] md:px-[141px] md:py-[16px] md:text-[1rem]"
+        className="font-HappinessB bg-primary w-full rounded-[50px] py-[8px] text-center text-[0.875rem] whitespace-nowrap text-[#fff] md:w-[386px] md:py-[16px] md:text-[1rem]"
       >
         {testBtnText}
       </Link>
