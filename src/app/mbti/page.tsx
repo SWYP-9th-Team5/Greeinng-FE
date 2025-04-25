d8cbd8f72dc566f8ee5f345321b9998cb154fb44
Kyeong Hoon Chu <khchu1220@naver.com>
2025-04-25 09:55:13 +0900

    style: mbti index 페이지 줄 바꿈 방식 변경

--- src/app/mbti/page.tsx
+++ src/app/mbti/page.tsx
@@ -24,14 +24,9 @@ export default function MbtiPage() {
     <>
       <h1 className="title1 text-primary mb-[24px] md:mb-[40px]">{title}</h1>
       <div className="mb-[24px] flex flex-col items-center justify-center md:mb-[0px]">
-        {introText.split('\n').map((text, index) => (
-          <p
-            key={index}
-            className="font-HappinessR body1 tracking-[-0.32px] text-[#333]"
-          >
-            {text}
-          </p>
-        ))}
+        <p className="font-HappinessR body1 text-center tracking-[-0.32px] whitespace-pre-line text-[#333]">
+          {introText}
+        </p>
       </div>
       <div className="max-xxs:w-full mb-[18px] w-[350px] md:mb-[7px] md:w-[400px]">
         <Image
