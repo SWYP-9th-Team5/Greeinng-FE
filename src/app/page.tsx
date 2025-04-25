5bf1f3017974a051ea84ffa57196cbe316d0f083
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2025-04-25 16:06:38 +0900

    feat: main page 일부 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
+import TabComponent from '@/components/features/main/Tabbarcomponent';
+
+const PCHome = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-start overflow-x-hidden"
+      style={{ height: 'calc(100vh - 80px)' }}
+    >
+      <div className="title1 text-text2 flex min-h-[600px] w-full flex-col items-start justify-center bg-[#e9e9e9]">
+        식물을 키우는 모든 순간
+        <br />
+        플랫폼에서
+      </div>
+      <div className="text-text2 bg-background flex min-h-[600px] w-full flex-col items-center justify-center">
+        <p className="text-text2 title1 mb-7">COMMUNITY</p>
+        <TabComponent />
+      </div>
+    </div>
+  );
+};
+const MBHome = () => {
+  return (
+    <div
+      className="flex flex-col items-center justify-center"
+      style={{ height: 'calc(100vh - 80px)' }}
+    >
+      main
+    </div>
+  );
+};
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
+    <>
+      <div className="block md:hidden">
+        <MBHome />
+      </div>
+      <div className="hidden md:block">
+        <PCHome />
+      </div>
+    </>
   );
 }
