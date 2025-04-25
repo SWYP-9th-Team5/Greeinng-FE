f0e55372498cf683298031fa3cecb974182be51b
Kyeong Hoon Chu <khchu1220@naver.com>
2025-04-25 15:40:03 +0900

    style: button 컴포넌트화

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,3 +1,38 @@
+import Button from '@components/common/Button';
+
 export default function CommunityPage() {
-  return <div>Community</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <Button color="primary">테스트 다시하기</Button>
+        <Button color="secondary">버튼</Button>
+        <Button color="point">버튼</Button>
+      </div>
+      <div>
+        <Button size="md" color="primary">
+          테스트 다시하기
+        </Button>
+        <Button size="md" color="secondary">
+          버튼
+        </Button>
+        <Button size="md" color="point">
+          버튼
+        </Button>
+        <Button size="md" color="gray">
+          버튼
+        </Button>
+      </div>
+      <div>
+        <Button size="sm" color="primary">
+          테스트 다시하기
+        </Button>
+        <Button size="sm" color="secondary">
+          버튼
+        </Button>
+        <Button size="sm" color="point">
+          버튼
+        </Button>
+      </div>
+    </div>
+  );
 }
