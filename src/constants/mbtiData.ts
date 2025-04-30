bdbcdb0bbe7d94957f919f766304ad36aca1b143
Kyeong Hoon Chu <khchu1220@naver.com>
2025-04-30 20:03:29 +0900

    fix: mbti 이미지 경로 수정

--- src/constants/mbtiData.ts
+++ src/constants/mbtiData.ts
@@ -27,22 +27,22 @@ export const MOBILE_MBTI_QUESTION_LIST = [
     question: 'Q1. 당신은\n 어떤 햇빛 아래에서 더 잘 자라나요?',
     aTypeAnswerText: '반짝반짝! 사람들과\n 어울릴 때 활짝 피어요',
     bTypeAnswerText: '조용한 그늘 아래에서\n혼자 무럭무럭 크는게 좋아요',
-    aTypeSrc: '/images/Q1-1@2x.png',
-    bTypeSrc: '/images/Q1-2@2x.png',
+    aTypeSrc: '/images/mbti/question/Q1-1@2x.png',
+    bTypeSrc: '/images/mbti/question/Q1-2@2x.png',
   },
   {
     question: 'Q2. 꿈꾸는 화분이 있다면?',
     aTypeAnswerText: '하늘을 날며\n빛을 모으는 마법 화분',
     bTypeAnswerText: '바닥에 안정감 있게 놓인\n튼튼한 도자기 화분',
-    aTypeSrc: '/images/Q2-1@2x.png',
-    bTypeSrc: '/images/Q2-2@2x.png',
+    aTypeSrc: '/images/mbti/question/Q2-1@2x.png',
+    bTypeSrc: '/images/mbti/question/Q2-2@2x.png',
   },
   {
     question: 'Q3. 식물 친구가 시들었을 때 나는...?',
     aTypeAnswerText: '조명, 토양, 습도 체크!\n원인 분석부터 합니다',
     bTypeAnswerText: '말 걸어주고 부드럽게 닦아줘요\n괜찮아?ㅠㅠ',
-    aTypeSrc: '/images/Q3-1@2x.png',
-    bTypeSrc: '/images/Q3-2@2x.png',
+    aTypeSrc: '/images/mbti/question/Q3-1@2x.png',
+    bTypeSrc: '/images/mbti/question/Q3-2@2x.png',
   },
 ];
 
