29c6609f2e7d2e124af8d0c048d9e58e6ce98607
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2025-05-10 17:55:26 +0900

    fix: 사이드바 로그아웃 반영

--- src/components/layout/Sidebar/SidebarHeader/SidebarHeader.tsx
+++ src/components/layout/Sidebar/SidebarHeader/SidebarHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useAuthStore } from '@/stores/useAuthStore';
 import { useSidebarStore } from '@/stores/useSidebarStore';
 import { cn } from '@/utils/cn';
 import Image from 'next/image';
@@ -8,7 +9,30 @@ import Link from 'next/link';
 export default function SidebarHeader() {
   const { actionSidebarClose } = useSidebarStore();
 
-  return (
+  const { isLoggedIn, logout } = useAuthStore();
+
+  const handleLogout = () => {
+    logout();
+    localStorage.removeItem('token');
+    actionSidebarClose();
+    window.location.href = '/';
+  };
+
+  return isLoggedIn ? (
+    <div className={cn('mb-[2rem] flex items-center justify-between')}>
+      <button
+        onClick={handleLogout}
+        className="font-NanumSquareRoundB flex items-center tracking-[-0.02rem] text-[#666]"
+        aria-label="로그아웃 버튼"
+      >
+        <Image src="/icons/person.svg" alt="" width={18} height={18} />
+        로그아웃
+      </button>
+      <button onClick={actionSidebarClose} aria-label="사이드바 닫기">
+        <Image src="/icons/close.svg" alt="" width={24} height={24} />
+      </button>
+    </div>
+  ) : (
     <div className={cn('mb-[2rem] flex items-center justify-between')}>
       <Link
         className="font-NanumSquareRoundB flex items-center tracking-[-0.02rem] text-[#666]"
