1e115020d0425e4b6258959192ebebceae2014e1
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2025-05-14 14:32:42 +0900

    refactor: popup 구조 하나로 수정

--- src/components/common/Popup/Popup.tsx
+++ src/components/common/Popup/Popup.tsx
@@ -1,8 +1,10 @@
 'use client';
 
 import { usePopupStore } from '@/stores/usePopupStore';
+import { cn } from '@/utils/cn';
 
-import BasePopup from '@components/popup/BasePopup';
+import Button from '@components/common/Button';
+import FocusTrap from '@components/common/FocusTrap';
 
 export default function Popup() {
   const {
@@ -11,7 +13,7 @@ export default function Popup() {
     description,
     confirmText,
     cancelText,
-    mode,
+    mode = 'double',
     className,
     onConfirm,
     onCancel,
@@ -21,21 +23,54 @@ export default function Popup() {
   if (!isOpen) return null;
 
   return (
-    <BasePopup
-      title={title}
-      description={description}
-      confirmText={confirmText}
-      cancelText={cancelText}
-      mode={mode}
-      className={className}
-      onConfirm={() => {
-        onConfirm();
-        closePopup();
-      }}
-      onCancel={() => {
-        if (onCancel) onCancel();
-        closePopup();
-      }}
-    />
+    <div
+      className="fixed inset-0 z-[9999] flex items-center justify-center"
+      style={{ backgroundColor: 'rgba(51, 51, 51, 0.3)' }}
+      role="dialog"
+      aria-modal="true"
+    >
+      <FocusTrap
+        isActive
+        isAutoFocus={false}
+        isRestoreFocus
+        className={cn(
+          'aspect-[320/173] w-[320px] max-w-[320px] rounded-[1.25rem] bg-[#FFF] p-4 text-center md:aspect-[400/209] md:w-[400px] md:max-w-[400px] md:p-6',
+          className,
+        )}
+      >
+        <p className="title2 text-text2 mb-4">{title}</p>
+        {description && (
+          <p className="subTitle whitespace-pre-line text-[#666]">
+            {description}
+          </p>
+        )}
+
+        <div className="mt-12 flex justify-center gap-2">
+          <Button
+            size="md"
+            color="primary"
+            onClick={() => {
+              onConfirm();
+              closePopup();
+            }}
+            className={mode === 'single' ? 'w-[120px]' : ''}
+          >
+            {confirmText}
+          </Button>
+          {mode !== 'single' && onCancel && (
+            <Button
+              size="md"
+              color="gray"
+              onClick={() => {
+                onCancel();
+                closePopup();
+              }}
+            >
+              {cancelText}
+            </Button>
+          )}
+        </div>
+      </FocusTrap>
+    </div>
   );
 }
