fab7580231a1ae80e4b4b87b0c55d5d33d5e8ef2
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2025-05-14 16:24:13 +0900

    feat: 구글 로그인

---
+++ src/app/login/googlelogin.tsx
@@ -0,0 +1,38 @@
+import api from '@/apis/api-config';
+import { useAuthStore } from '@/stores/useAuthStore';
+
+export const googleLoginButton = async () => {
+  const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
+  const REDIRECT_URI = 'http://localhost:3000/login/google';
+  const SCOPE = 'email profile';
+
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=${SCOPE}`;
+
+  window.location.href = googleAuthUrl;
+};
+
+export const googleLogin = async () => {
+  try {
+    const url = new URL(window.location.href);
+    const code = url.searchParams.get('code');
+
+    if (!code) {
+      alert('구글 인가 코드가 없습니다.');
+      return;
+    }
+
+    const res = await api.post<{
+      data: { accessToken: string; newJoin: boolean };
+    }>('/api/login/google', { code });
+
+    const { accessToken } = res.data;
+
+    localStorage.setItem('token', accessToken);
+    useAuthStore.getState().login(accessToken);
+
+    window.location.href = '/'; // 로그인 성공 시 메인으로 이동
+  } catch (err) {
+    console.error('[googleLogin] 에러 발생:', err);
+    alert('구글 로그인에 실패했습니다.');
+  }
+};
