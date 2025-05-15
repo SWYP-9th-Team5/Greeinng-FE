5c468b753fd5767dfe695701b51f2cf332481b15
Kyeong Hoon Chu <khchu1220@naver.com>
2025-05-16 07:30:14 +0900

    setup: image cloud url 설정

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: ['kr.object.ncloudstorage.com'],
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
