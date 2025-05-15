532c88074c4dc3be400bf77dc77c14b177325ed0
KwonHaeJin <103015007+KwonHaeJin@users.noreply.github.com>
2025-05-15 17:51:45 +0900

    feat: 커뮤니티 페이지 api 연결

--- src/components/features/All/CommunityTable.tsx
+++ src/components/features/All/CommunityTable.tsx
@@ -1,149 +1,80 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 import { useTabStore } from '@/stores/useTabStore';
 
+import api from '@apis/api-config';
+
 import { CommunityList } from '../community/List';
+import { Nocontents } from '../community/common/NoContents/NoContents';
+import { PostItem } from '../main/Community/MainTabbar';
 
-const dummyData = {
-  QnA: [
-    {
-      id: 1,
-      title:
-        '갑자기 잎이 다 떨어졌어요... 어떡하죠..zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz.',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll 대한으로 길이 보전하세',
-      likecount: 26,
-      like: true,
-      commentcount: 21,
-    },
-    {
-      id: 2,
-      title: '흙 위에 하얀 곰팡이처럼 뭐가 생겼어요ㅜㅜ',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 24,
-      like: true,
-      commentcount: 21,
-    },
-    {
-      id: 3,
-      title: '직광이 잘 드는 곳, 어떤 식물이 좋을까요?',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 20,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 4,
-      title: '새싹이 안 자라요ㅜ',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 18,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 5,
-      title: '분갈이 언제 해줘야 하나요?????????????????????????',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 15,
-      like: false,
-      commentcount: 21,
-    },
-  ],
+interface CommunityResponse {
+  data: PostItem[];
+  pagination: {
+    totalCounts: number;
+    totalPages: number;
+    currentPage: number;
+    pageSize: number;
+  };
+}
 
-  자유게시판: [
-    {
-      id: 1,
-      title: '동해물과 백두산이 마르고2',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 26,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 2,
-      title: '동해물과 백두산이 마르고2',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 24,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 3,
-      title:
-        '동해물과 ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss마르고2',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 20,
-      like: true,
-      commentcount: 21,
-    },
-    {
-      id: 4,
-      title: '동해물과 백두산이 마르고2',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 18,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 5,
-      title: '동해물과 백두산이 마르고2',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 15,
-      like: false,
-      commentcount: 21,
-    },
-  ],
-
-  나눔: [
-    {
-      id: 1,
-      title: '동해물과 백두산이 마르고3',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 26,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 2,
-      title: '동해물과 백두산이 마르고3',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 24,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 3,
-      title: '동해물과 백두산이 마르고3',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 20,
-      like: false,
-      commentcount: 21,
-    },
-    {
-      id: 4,
-      title: '동해물과 백두산이 마르고3',
-      detail:
-        '동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세 무궁화 삼천리 화려강산 대한 사람 대한으로 길이 보전하세',
-      likecount: 18,
-      like: true,
-      commentcount: 21,
-    },
-  ],
+const categoryMap: Record<string, string> = {
+  QnA: 'QnA',
+  자유게시판: 'FREE_BULLETIN_BOARD',
+  나눔: 'SHARING',
 };
 
 export const CommunityTable = () => {
   const { activeTab } = useTabStore();
-  const data = dummyData[activeTab as keyof typeof dummyData];
+  const [data, setData] = useState<PostItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  return <CommunityList data={data} label={activeTab} />;
+  useEffect(() => {
+    // 탭 변경시 현재 페이지 초기화
+    setCurrentPage(1);
+  }, [activeTab]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const res = await api.get<CommunityResponse>('/api/posts', {
+          params: {
+            category: categoryMap[activeTab],
+            pageNumber: currentPage,
+            pageSize: 5, // 한 페이지 당 5개로 고정
+          },
+        });
+        setData(res.data);
+        setTotalPages(res.pagination.totalPages);
+      } catch (err) {
+        console.error('커뮤니티 데이터 불러오기 실패:', err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, [activeTab, currentPage]);
+
+  return (
+    <>
+      {isLoading ? (
+        <p className="py-8 text-center">로딩 중...</p> // 데이터 로딩 화면
+      ) : data.length === 0 ? (
+        <Nocontents /> // 데이터 없을 때 화면
+      ) : (
+        <CommunityList
+          data={data}
+          label={activeTab}
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageClick={(page) => setCurrentPage(page)}
+        />
+      )}
+    </>
+  );
 };
